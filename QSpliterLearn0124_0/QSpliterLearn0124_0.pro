bd89f10263c45b06dee57741bac6e8ebec5e82fd
hww <1486614505@qq.com>
2022-01-24 23:18:21 +0800

    Update first example of QSplitterLearn1124_0

--- QSpliterLearn0124_0/QSpliterLearn0124_0.pro
+++ QSpliterLearn0124_0/QSpliterLearn0124_0.pro
@@ -16,14 +16,11 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    main.cpp \
-    widget.cpp
+    main.cpp
 
-HEADERS += \
-    widget.h
+HEADERS +=
 
-FORMS += \
-    widget.ui
+FORMS +=
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- QSpliterLearn0124_0/main.cpp
+++ QSpliterLearn0124_0/main.cpp
@@ -1,11 +1,47 @@
-#include "widget.h"
+#include "qsplitter.h"
+#include "qpushbutton.h"
+#include "QVBoxLayout"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
+
+    QWidget w;
+    QSplitter *ps = new QSplitter(Qt::Vertical);
+    QPushButton *pb = new QPushButton("AAA");
+    QPushButton *pb1 = new QPushButton("BBB");
+    QPushButton *pb2 = new QPushButton("CCC");
+    QPushButton *pb3 = new QPushButton("DDD");
+
+    pb1->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed); //disable size policy
+    pb->setMaximumSize(175, 55); //Max and Min do also work
+    ps->addWidget(pb);
+    ps->addWidget(pb1);
+    ps->addWidget(pb2);
+    ps->addWidget(pb3);
+    //set border of splitter
+    ps->setFrameShadow(QFrame::Sunken);
+    ps->setFrameShape(QFrame::Box);
+    ps->setLineWidth(5);
+    //children can't be collapsible
+    ps->setChildrenCollapsible(false);
+    //adjust size dynamicly
+    ps->setOpaqueResize(true);
+
+    ps->setStretchFactor(0, 1);
+    ps->setStretchFactor(1, 2);
+    ps->setStretchFactor(2, 2);
+
+    ps->setHandleWidth(5);
+
+    ps->setStyleSheet("QSplitter::handle{background-color:blue}");
+    QVBoxLayout *pv = new QVBoxLayout;
+    pv->addWidget(ps);
+    w.setLayout(pv);
+    w.resize(180, 200);
     w.show();
+
     return a.exec();
 }
